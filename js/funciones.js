359651e9d0273cbcc68c27ab7e64033a4baab05b
ricardobe1 <102460673+ricardobe1@users.noreply.github.com>
2022-03-31 02:17:54 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+function gasolina(){
+    var pasos=null;
+    pasos =document.getElementById("igasolina").value;
+    var calorias=pasos*3;
+    alert("El comustible gastado es: "+combustible+"aproximadamente");
+    }
+
+
+
